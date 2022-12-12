c823a6ebe0c818254d7513f6f38728e8b518596a
Douglas Moura <douglas.ademoura@gmail.com>
2022-12-12 16:33:12 -0300

    fix: adiciona node-fetch para usar fetch nos testes

--- .jest/setup.js
+++ .jest/setup.js
@@ -2,8 +2,10 @@ import * as React from 'react'
 import { setGlobalConfig } from '@storybook/testing-react'
 import { toHaveNoViolations } from 'jest-axe'
 import * as globalStorybookConfig from '../.storybook/preview'
+import fetch from 'node-fetch'
 
 setGlobalConfig(globalStorybookConfig)
 expect.extend(toHaveNoViolations)
 
 global.React = React
+global.fetch = fetch
