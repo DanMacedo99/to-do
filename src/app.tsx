9f5b93da452c8e6d8ec01f286938456f257a5417
Douglas Moura <douglas.ademoura@gmail.com>
2022-12-12 14:31:11 -0300

    feat: cria formulário para adicionar tarefas

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,39 @@
+import { useState } from 'react'
+
+type Task = {
+  id: string
+  name: string
+}
+
 export const App: React.FC = () => {
-  return <h1>Hello, World!</h1>
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          const formData = new FormData(e.currentTarget)
+          setTasks((currentState) => [
+            ...currentState,
+            {
+              id: 'asdfafq',
+              name: formData.get('tasks')?.toString() || '',
+            },
+          ])
+        }}
+      >
+        <label htmlFor="task">Adicionar tarefa:</label>
+        <br />
+        <input name="task" id="task" />
+        <button type="submit">+</button>
+      </form>
+
+      <ul>
+        {tasks?.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
